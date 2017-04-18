6959379eec17e6e5e9eb3c56927358a601acb48b
Máté Kovács <kovacsur10@gmail.com>
2017-04-18 12:59:26 +0200

    Dummy GPS upgrade.

--- mind.py
+++ mind.py
@@ -1,23 +1,25 @@
 import time
-
 import engine
 import gps
 
 class Mind:
-    def __init__(self):
-        self.engine = engine.Engine()
-        self.gps = gps.GPS()
-        self.gps.start()
-        
-    def getLocation(self):
-        print self.gps.get()
+		def __init__(self):
+				self.engine = engine.Engine()
+				self.gps = gps.GPS()
+				self.gps.start()
+				
+		def getLocation(self):
+				print self.gps.get()
 
-    def test(self):
-        self.engine.moveForward(60)
-        time.sleep(1)
-        self.engine.stop()
-        time.sleep(2)
-        self.engine.cleanUp()
-        
+		def test(self):
+				self.engine.moveForward(60)
+				time.sleep(1)
+				self.engine.stop()
+				time.sleep(2)
+				for i in range(1,100):
+					self.getLocation()
+					time.sleep(1)
+				self.engine.cleanUp()
+				
 mind = Mind()
 mind.test()
--- myserial.py
+++ myserial.py
@@ -8,11 +8,40 @@ EIGHTBITS = "EIGHTBITS"
 class Serial:
 	def __init__(self, port, baudrate, parity, stopbits, bytesize, timeout):
 		self.time = random.randint(1,2200)
+		self.lastNorth = 90
+		self.lastEast = 70
 		print "Serial init"
 		
+	# 1/100 esellyel lehet nagyon rossz
+	# 1/8 esellyel lehet rossz a hatarokon belul
+	# amugy pedig viszonylag pontos eredmenyt ad
 	def readline(self):
-		north = random.randint(80,99)
-		east = random.randint(60, 80)
-		time.sleep(0.8)
+		bad = random.randint(1,100)
+		if bad == 1:
+			north = random.randint(1,99)
+			east = random.randint(1, 99)
+		else:
+			bad = random.randint(1,8)
+			if bad == 1:
+				north = random.randint(80,99)
+				east = random.randint(60, 80)
+			else:
+				minNorth = self.lastNorth - 3
+				maxNorth = self.lastNorth + 3
+				minEast = self.lastEast - 3
+				maxEast = self.lastEast + 3
+				if minNorth < 80:
+					minNorth = 80
+				if maxNorth > 99:
+					maxNorth = 99
+				if minEast < 60:
+					minEast = 60
+				if maxEast > 80:
+					maxEast = 80
+				north = random.randint(minNorth, maxNorth)
+				east = random.randint(minEast, maxEast)
+		time.sleep(0.5)
 		self.time = self.time + 1
+		self.lastNorth = north
+		self.lastEast = east
 		return "$GPGGA,{t}.000,4728.43{no},N,01903.47{ea},E,1,5,1.69,73.8,M,41.1,M,,*60".format(t=self.time, no=north, ea=east)
