edfd3207d8e93b7643cfe07739fc59a2e3b8fe76
Máté Kovács <kovacsur10@gmail.com>
2017-04-26 17:19:11 +0200

    Deque was added.

--- mind.py
+++ mind.py
@@ -2,17 +2,20 @@ import time
 import engine
 import gps
 from point import Point
+from collections import deque
 
 class Mind:
 	checkPoint = None
-	currentPosition = None
+	positionQueue = None # for average counting and movement towards the check point
+	maxPositions = 5
 
 	def __init__(self):
 		self.engine = engine.Engine()
 		self.gps = gps.GPS()
 		self.gps.start()
 		self.setNextPoint(Point(None, None))
-		self.currentPosition = None
+		self.positionQueue = deque()
+		self.maxPositions = 5
 		
 	def setNextPoint(self, checkPoint):
 		self.checkPoint = checkPoint
@@ -20,19 +23,25 @@ class Mind:
 	def getLocation(self):
 		delta = self.gps.get()
 		print delta
-		self.currentPosition = delta[0]
-		if self.currentPosition != None:
-			self.currentPosition.print_()
+		if len(self.positionQueue) == self.maxPositions:
+			self.positionQueue.popleft()
+		self.positionQueue.append(delta[0])
+		
+		if len(self.positionQueue) != 0:
+			self.positionQueue[-1].print_()
+			if self.checkPoint != None:
+				self.positionQueue[-1].getAngleTo(self.checkPoint)
 
 	def test(self):
-		# self.engine.moveForward(60)
-		# time.sleep(1)
-		# self.engine.stop()
-		# time.sleep(2)
+		self.engine.moveForward(60)
+		time.sleep(1)
+		self.engine.stop()
+		time.sleep(2)
 		for i in range(1,100):
 			self.getLocation()
 			time.sleep(1)
 		self.engine.cleanUp()
 
 mind = Mind()
+mind.setNextPoint(Point(19.034780, 47.284399))
 mind.test()
